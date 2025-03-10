5d477d009b8a362eae8d3e6dd7defa6d696862da
Priyansh Verma <verma.priyansh444@gmail.com>
2025-03-11 01:49:21 +0530

    added user pfp option

--- src/components/profile/userBio/UserBio.jsx
+++ src/components/profile/userBio/UserBio.jsx
@@ -14,6 +14,8 @@ const UserBio = () => {
     const [userData, setUserData] = useState({});
     const [updatedBio, setUpdatedBio] = useState("");
     const [isEditingBio, setIsEditingBio] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [isImageUploadVisible, setIsImageUploadVisible] = useState(false);
 
     useEffect(() => {
         fetchUserData().then((data) => {
@@ -62,80 +64,132 @@ const UserBio = () => {
         }
     };
 
+    const handleImageUpload = async () => {
+        if (!selectedImage) {
+            showToast("warning", "Please select an image to upload.");
+            return;
+        }
+        const formData = new FormData();
+        formData.append("image", selectedImage);
+    
+        try {
+            const response = await axios.put(
+                `${backendUrl}/api/users/upload/${userData._id}`,
+                formData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+    
+            if (response.status === 200) {
+                showToast("success", "Image uploaded successfully");
+                setUserData({ ...userData, userImage: response.data.userImage });
+                setIsImageUploadVisible(false);
+            }
+        } catch (error) {
+            console.error("Error uploading image", error);
+            showToast("error", "Error uploading image");
+        }
+    };
+
     return (
         <div className="flex flex-col justify-center items-center h-full">
-            <div className="flex justify-center m-4 p-2">
+            <div className="flex justify-center m-2 p-2">
                 <Image
                     borderRadius="full"
                     boxSize="150px"
                     as={Avatar}
-                    src={userData.userImg}
+                    src={`${backendUrl}${userData.userImage}`} // Ensure the correct URL is used
                 />
             </div>
-            {/* <div className="flex flex-col justify-center items-center">
-                <div className="mx-3 justify-center items-center flex space-x-4">
-                    <Tag>
-                        {" "}
-                        <GiPodiumSecond className="w-5 h-5" /> 4{" "}
-                    </Tag>
-                    <Tag>
-                        {" "}
-                        <GiPodiumWinner className="w-8 h-8" /> 4{" "}
-                    </Tag>
-                    <Tag>
-                        {" "}
-                        <GiPodiumThird className="w-5 h-5" /> 4{" "}
-                    </Tag>
-                </div>
-            </div> */}
             <div className="w-full flex flex-col items-center justify-center mx-1">
-                <p className="w-fit mx-2 mt-3 my-2 font-bold text-2xl"> Bio </p>
+                <p className="w-fit mx-2 my-2 font-bold text-3xl"> User Bio </p>
                 {isEditingBio ? (
                     <div className="w-full flex flex-col justify-center items-center">
                         <textarea
-                            className="w-full p-2 px-3 h-28 mt-1 shadow appearance-none border border-gray-700 hover:border-gray-400 bg-neutral-900 text-white rounded leading-tight focus:outline-none focus:shadow-outline"
+                            className="w-full p-2 px-3 h-24 mt-1 shadow appearance-none border border-gray-700 hover:border-gray-400 bg-neutral-900 text-white rounded leading-tight focus:outline-none focus:shadow-outline"
                             name="text"
                             type="text"
                             value={updatedBio}
                             onChange={(e) => setUpdatedBio(e.target.value)}
                         />
-                        <button
-                            className="w-1/4 mt-1 py-1 bg-teal-800 hover:bg-teal-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
-                            type="button"
-                            onClick={handleUpdateBio}
-                        >
-                            Update
-                        </button>
-                        <button
-                            className="w-1/4 mt-1 py-1 bg-red-800 hover:bg-red-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
-                            type="button"
-                            onClick={() => {
-                                setIsEditingBio(false);
-                                setUpdatedBio(userData.bio);
-                            }}
-                        >
-                            Cancel
-                        </button>
+                        <div className="flex space-x-2 mt-2">
+                            <button
+                                className="p-1 bg-teal-800 hover:bg-teal-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
+                                type="button"
+                                onClick={handleUpdateBio}
+                            >
+                                Upload
+                            </button>
+                            <button
+                                className="p-1 bg-red-800 hover:bg-red-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
+                                type="button"
+                                onClick={() => {
+                                    setIsEditingBio(false);
+                                    setUpdatedBio(userData.bio);
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 ) : (
                     <div className="flex justify-center text-center items-center flex-col">
-                        <Tag fontWeight="" colorScheme="" className="mt-4">
+                        <Tag fontWeight="" colorScheme="" className="">
                             {userData.bio}.
                         </Tag>
                         <Tag
                             fontWeight="bold"
                             colorScheme="gray"
-                            className="w-fit cursor-pointer mx-2 my-2 mt-5"
+                            className="w-fit cursor-pointer mt-20"
                             onClick={() => setIsEditingBio(true)}
                         >
-                            Edit
+                            Edit Bio
                             <AiFillEdit className="w-4 h-4" />
                         </Tag>
                     </div>
                 )}
             </div>
+            {!isImageUploadVisible && (
+                <button
+                    className="mt-12 p-1 bg-white text-black text-sm font-bold rounded focus:outline-none focus:shadow-outline"
+                    type="button"
+                    onClick={() => setIsImageUploadVisible(true)}
+                >
+                    Update PFP
+                </button>
+            )}
+            {isImageUploadVisible && (
+                <div className="flex flex-col items-center mt-4">
+                    <input
+                        type="file"
+                        accept="image/*"
+                        className="border border-gray-700 rounded cursor-pointer"
+                        onChange={(e) => setSelectedImage(e.target.files[0])}
+                    />
+                    <div className="flex mt-2 space-x-2">
+                        <button
+                            className="px-1 py-1 bg-teal-800 hover:bg-teal-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
+                            type="button"
+                            onClick={handleImageUpload}
+                        >
+                            Upload
+                        </button>
+                        <button
+                            className="px-1 py-1 bg-red-800 hover:bg-red-700 text-white text-xs font-bold rounded focus:outline-none focus:shadow-outline"
+                            type="button"
+                            onClick={() => setIsImageUploadVisible(false)}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default UserBio
+export default UserBio;
